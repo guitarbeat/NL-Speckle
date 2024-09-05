02f8327c3b8c4a942d237f3a364687cd56e4618f
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2024-09-05 12:27:48 -0500

    fixes

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"aaronlorenzowoods@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
+enableXsrfProtection=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
